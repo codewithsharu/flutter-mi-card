d96d5e751486f0b072899e745e6610dac65d8d4f
Shareen <codewithsharu@gmail.com>
2024-05-22 13:23:35 +0530

    blue-t

--- lib/main.dart
+++ lib/main.dart
@@ -10,111 +10,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(), // Setting dark theme
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.lightBlue, // Setting primary color to light blue
+      ),
       home: Scaffold(
-        backgroundColor: Colors.grey[900], // Background color for dark theme
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/profile.jpg'),
-              ),
-              Text(
-                'Shareen Dandasi',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.lightBlue.shade100, Colors.lightBlueAccent],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/profile.jpg'),
                 ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.teal.shade100,
-                  fontWeight: FontWeight.bold,
+                SizedBox(height: 10),
+                Text(
+                  'Shareen Dandasi',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 20.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.grey[800], // Adjusted container color for dark theme
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.location_on,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'Tekkali',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
+                SizedBox(
+                  height: 20.0,
+                  child: Divider(
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.grey[800], // Adjusted container color for dark theme
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.mail_outline,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'shareen@gmail.com',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.grey[800], // Adjusted container color for dark theme
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+91 78XXXXXX',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ],
+                buildInfoContainer(Icons.location_on, 'Tekkali'),
+                buildInfoContainer(Icons.mail_outline, 'shareen@gmail.com'),
+                buildInfoContainer(Icons.phone, '+91 78XXXXXX'),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildInfoContainer(IconData icon, String text) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.3),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+          ),
+          SizedBox(width: 10.0),
+          Text(
+            text,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          )
+        ],
       ),
     );
   }
